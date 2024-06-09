7c7850f51496b92978ebea3f729f003d55009910
sashaTribe <sasha555.tribe@gmail.com>
2024-06-09 23:22:41 +0100

    added a method that gets all the colors needed for a room

--- src/wall.ts
+++ src/wall.ts
@@ -1,6 +1,8 @@
 import inquirer from 'inquirer';
 
-
+/**
+ * This section is the Wall layer
+ */
 class Wall {
     width: number;
     height: number; 
@@ -49,81 +51,109 @@ class Wall {
 
 }
 
-
-let measurements : number[][] = [];
-
-const wallInfo = await inquirer.prompt([
-    {
-        type: 'string',
-        name: 'color',
-        message: 'What color would you like this wall to be?'
-    },
-    {
-        type: 'number',
-        name: 'length',
-        message: 'Type in the length of your wall: '
-    },
-    {
-        type: 'number',
-        name: 'width',
-        message: 'Type in the width of your wall: ' 
-    },
-    {
-        type: 'number',
-        name: 'numOfObjects',
-        message: 'Do you have any removable objects on the wall (E.g.Doors, windows, tiles etc.?If so how many? '
-    }
-])
-
-let height:number = wallInfo.length;
-let width:number = wallInfo.width;
-let color:string = wallInfo.color;
-
-let numOfObjects:number = wallInfo.numOfObjects;
-let counter: number = numOfObjects;
-while (counter > 0){
- 
-    const extraMeasurementInfo = await inquirer.prompt([
-        {
-            type: 'number',
-            name: 'height',
-            message: 'Height of object: '
-        },
-        {
-            type: 'number',
-            name: 'width',
-            message: 'Width of object: '
-        }
-    ])
-    
-    let measurementPair : number[] = [extraMeasurementInfo.height, extraMeasurementInfo.width];
-    measurements.push(measurementPair);
-    counter -= 1;
-}
-
-let wall: Wall = new Wall(width, height, color, measurements)
-wall.describe();
-
-
-
-
-
 class Room {
     name: string;
-    walls: [Wall];
+    walls: Wall[];
     
 
-    constructor(name: string, walls: [Wall]){
+    constructor(name: string, walls: Wall[]){
         this.name = name;
         this.walls = walls;
     }
 
-    addWall(wall: Wall):void{
-        this.walls.push(wall);
+    getColorsNeeded():string[] {
+        let colorList:string[] = []
+        for (var wall of walls){
+            let temp_color:string = wall.getColor()
+            if(!colorList.includes(temp_color)){
+                colorList.push(temp_color)
+            }
+        }
+        return colorList
+
     }
 
 
 }
+
+const roomInfo = await inquirer.prompt([
+    {
+        type: 'string',
+        name: 'roomName',
+        message: 'What is the name of this room?'
+    },
+
+    {
+        type: 'number',
+        name: 'numOfWalls',
+        message: 'How many walls are in this room?'
+    },
+
+])
+
+let roomName: string = roomInfo.roomName;
+let numOfWalls : number = roomInfo.numOfWalls;
+let walls : Wall[] = [];
+for (let i=0; i < numOfWalls; i++) {
+    
+        let measurements : number[][] = [];
+
+        const wallInfo = await inquirer.prompt([
+            {
+                type: 'string',
+                name: 'color',
+                message: 'What color would you like this wall to be?'
+            },
+            {
+                type: 'number',
+                name: 'length',
+                message: 'Type in the length of your wall: '
+            },
+            {
+                type: 'number',
+                name: 'width',
+                message: 'Type in the width of your wall: ' 
+            },
+            {
+                type: 'number',
+                name: 'numOfObjects',
+                message: 'Do you have any removable objects on the wall (E.g.Doors, windows, tiles etc.?If so how many? '
+            }
+        ])
+
+        let height:number = wallInfo.length;
+        let width:number = wallInfo.width;
+        let color:string = wallInfo.color;
+
+        let numOfObjects:number = wallInfo.numOfObjects;
+        let counter: number = numOfObjects;
+        while (counter > 0){
+        
+            const extraMeasurementInfo = await inquirer.prompt([
+                {
+                    type: 'number',
+                    name: 'height',
+                    message: 'Height of object: '
+                },
+                {
+                    type: 'number',
+                    name: 'width',
+                    message: 'Width of object: '
+                }
+            ])
+            
+            let measurementPair : number[] = [extraMeasurementInfo.height, extraMeasurementInfo.width];
+            measurements.push(measurementPair);
+            counter -= 1;
+        }
+        let wall: Wall = new Wall(width, height, color, measurements)
+        walls.push(wall);
+
+    }
+
+let room:Room = new Room(roomName, walls);
+
+
 
 
 interface PaintNeeded {
